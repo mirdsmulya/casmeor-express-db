5b67eed19a2dbc7d755f3fca06caa50ed4e60724
mirdan <syahidmirdan@gmal.com>
2020-05-12 16:11:18 +0700

    Add route for account select, save account, and delete account

--- routes.js
+++ routes.js
@@ -3,9 +3,12 @@
 module.exports = function(app) {
     var todoList = require('./controller');
 
-    app.route('/')
-        .get(todoList.index);
-
-    app.route('/users')
-        .get(todoList.users);
+    app.route('/account')
+        .get(todoList.account);
+    
+    app.route('/saveAccount')
+        .post(todoList.saveAccount);
+    
+    app.route('/deleteAccount')
+        .delete(todoList.deleteAccount);
 };
