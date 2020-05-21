c32b60b71d70a387cfece91524506e9dfc660147
mirdan <syahidmirdan@gmal.com>
2020-05-21 12:13:20 +0700

    Change file destination to specific file controller

--- routes.js
+++ routes.js
@@ -1,14 +1,28 @@
 'use strict';
 
 module.exports = function(app) {
-    var todoList = require('./controller');
+    var accountController = require('./controller/accountController');
+    var orderController = require('./controller/orderController');
 
     app.route('/account')
-        .get(todoList.account);
+        .get(accountController.account);
     
     app.route('/saveAccount')
-        .post(todoList.saveAccount);
+        .post(accountController.saveAccount);
     
     app.route('/deleteAccount')
-        .delete(todoList.deleteAccount);
+        .delete(accountController.deleteAccount);
+
+    app.route('/orders') 
+        .get(orderController.order);
+    
+    app.route('/saveOrder') 
+        .post(orderController.saveOrder);  
+    
+    app.route('/getItemOrder')
+        .get(orderController.getItemOrder);
+
+    app.route('/updateOrder')
+        .put(orderController.updateOrder);
+
 };
